2caf9b365f75f45a69e8d4b1688e61f73b10478d
ahmed nasser <ahmednas197@gmail.com>
2022-04-18 02:38:11 +0200

    finalize the setup of the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
-};
-
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
 const App = () => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, "Adopt me!"),
@@ -28,4 +21,4 @@ const App = () => {
         }),
     ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
