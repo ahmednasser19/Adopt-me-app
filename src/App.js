5a5f464ff06ee64b056cb4479bd4fe191ce7c51a
ahmed nasser <ahmednas197@gmail.com>
2022-04-19 17:48:57 +0200

    add details components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import SearchParams from './SearchParams';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { StrictMode } from 'react';
 import Details from './Details';
 
 const App = () => {
     return (
         <StrictMode>
-            <h1>lol</h1>
             <BrowserRouter>
-                <h1>Adopt me!</h1>
+                <header>
+                    <Link to='/'>
+                        Adopt me!
+                    </Link>
+                </header>
                 <Routes>
                     <Route path="/details/:id" element={<Details />} />
                     <Route path="/" element={<SearchParams />} />
--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import { Component } from 'react'
+import { useParams } from 'react-router-dom'
 
-export default function Details() {
-    return (
-        <div>Details</div>
-    )
+class Details extends Component {
+    //as the class is initialized run this constructor function
+    constructor(props) {
+        super(props);
+
+        this.state = { loading: true };
+    }
+    //run once it's equla useEffect( ()=>{} , [])
+    async componentDidMount() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+        );
+        const json = await res.json();
+        this.setState(Object.assign({ loading: false }, json.pets[0]));
+    }
+
+    render() {
+
+        if (this.state.loading) {
+            return (<h2>Loading</h2>)
+        }
+
+        const { animal, breed, city, state, description, name } = this.state;
+        return (
+            <div className='details'>
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} - {city},{state}</h2>
+                </div>
+                <button>Adopt {name}</button>
+                <p> {description}</p>
+            </div>
+        )
+    }
 }
+
+const WrappedDetails = () => {
+    const params = useParams();
+    return <Details params={params} />
+}
+export default WrappedDetails;
